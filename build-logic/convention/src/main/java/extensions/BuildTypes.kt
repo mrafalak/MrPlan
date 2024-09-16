35be56a4e3b111bed6463d7e935de37859dbcf02
mrafalak <maciej.rafalak.dev@gmail.com>
2024-09-16 14:57:56 +0200

    Add signing config to release app build

--- build-logic/convention/src/main/java/extensions/BuildTypes.kt
+++ build-logic/convention/src/main/java/extensions/BuildTypes.kt
@@ -1,15 +1,44 @@
 package extensions
 
-import com.android.build.api.dsl.CommonExtension
+import com.android.build.api.dsl.ApplicationExtension
+import com.android.build.gradle.LibraryExtension
 import config.ENABLE_ANALYTICS
 import config.ENABLE_CRASHLYTICS
 import org.gradle.api.Project
 
-internal fun Project.configureBuildTypes(commonExtension: CommonExtension<*, *, *, *, *, *>) {
-    commonExtension.apply {
+internal fun Project.configureAppBuildTypes(
+    appExt: ApplicationExtension,
+) {
+    appExt.apply {
         buildTypes {
             getByName("release") {
-                isMinifyEnabled = true
+                signingConfig = signingConfigs.getByName("release")
+                isMinifyEnabled = false
+                isShrinkResources = false
+                proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    file("proguard-rules.pro")
+                )
+                buildConfigField("boolean", ENABLE_CRASHLYTICS, "true")
+                buildConfigField("boolean", ENABLE_ANALYTICS, "true")
+            }
+            getByName("debug") {
+                isMinifyEnabled = false
+                isShrinkResources = false
+                buildConfigField("boolean", ENABLE_CRASHLYTICS, "false")
+                buildConfigField("boolean", ENABLE_ANALYTICS, "false")
+            }
+        }
+    }
+}
+
+internal fun Project.configureLibraryBuildTypes(
+    libExt: LibraryExtension,
+) {
+    libExt.apply {
+        buildTypes {
+            getByName("release") {
+                isMinifyEnabled = false
                 proguardFiles(
                     getDefaultProguardFile("proguard-android-optimize.txt"),
                     file("proguard-rules.pro")
--- build-logic/convention/src/main/java/plugins/AndroidAppConventionPlugin.kt
+++ build-logic/convention/src/main/java/plugins/AndroidAppConventionPlugin.kt
@@ -3,12 +3,13 @@ package plugins
 import com.android.build.api.dsl.ApplicationExtension
 import config.Config
 import extensions.configureAndroidKotlin
-import extensions.configureBuildTypes
+import extensions.configureAppBuildTypes
 import extensions.versionCatalog
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
+import java.util.Properties
 
 class AndroidAppConventionPlugin : Plugin<Project> {
 
@@ -27,14 +28,31 @@ class AndroidAppConventionPlugin : Plugin<Project> {
                 "implementation"(versionCatalog().findLibrary("hilt-android").get())
                 "ksp"(versionCatalog().findLibrary("hilt-android-compiler").get())
                 "implementation"(versionCatalog().findLibrary("timber-core").get())
-                "implementation"(platform(versionCatalog().findLibrary("google-firebase-bom").get()))
+                "implementation"(
+                    platform(
+                        versionCatalog().findLibrary("google-firebase-bom").get()
+                    )
+                )
                 "implementation"(versionCatalog().findLibrary("google-firebase-analytics").get())
                 "implementation"(versionCatalog().findLibrary("google-firebase-crashlytics").get())
             }
 
             extensions.configure<ApplicationExtension> {
+                val keystoreFile = project.rootProject.file("local.properties")
+                val properties = Properties()
+                properties.load(keystoreFile.inputStream())
+
+                signingConfigs {
+                    create("release") {
+                        storeFile = file(properties["KEYSTORE_PATH"] as String)
+                        storePassword = properties["KEYSTORE_PASSWORD"] as String
+                        keyAlias = properties["KEY_ALIAS"] as String
+                        keyPassword = properties["KEY_PASSWORD"] as String
+                    }
+                }
+
                 configureAndroidKotlin(this)
-                configureBuildTypes(this)
+                configureAppBuildTypes(this)
 
                 defaultConfig {
                     applicationId = Config.android.applicationId
